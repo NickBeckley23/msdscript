70a578d771d2d2cd877e7fb4311880a33927d5b2
nbeckley23 <nickbeckley@gmail.com>
2021-01-19 11:45:45 -0700

    Added cmdline and makefile

---
+++ msdscript/msdscript/cmdline.cpp
@@ -0,0 +1,31 @@
+//
+//  cmdline.cpp
+//  msdscript
+//
+//  Created by Nick Beckley on 1/19/21.
+//
+
+#include "cmdline.hpp"
+#include <iostream>
+
+
+void use_arguments(int argc,char * argv[]){
+    if(argc == 1) exit(1);
+    bool testSeen = false;
+    for(int i = 1; i < argc; i++){
+        std::string arg = argv[i];
+        if(arg == "--help"){
+            std::cout << "Arguments allowed: --help -- test\n";
+            exit(0);
+        }else if(arg == "--test" && testSeen == false){
+            std::cout << "Tests Passed\n";
+            testSeen = true;
+        }else if(arg == "--test" && testSeen == true){
+            std::cerr << "Tests already passed yo\n";
+            exit(1);
+        }else{
+            std::cerr << "Invalid argument\n";
+            exit(1);
+        }
+    }
+}
---
+++ msdscript/msdscript/cmdline.hpp
@@ -0,0 +1,18 @@
+//
+//  cmdline.hpp
+//  msdscript
+//
+//  Created by Nick Beckley on 1/19/21.
+//
+
+#ifndef cmdline_hpp
+#define cmdline_hpp
+
+#include <stdio.h>
+#include <string>
+
+void use_arguments(int argc, char * argv[]);
+
+#endif /* cmdline_hpp */
+
+
